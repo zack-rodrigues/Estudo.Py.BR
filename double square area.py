4745e5c9fa49ba2493b2f6a563b344a03de9aec0
Zack Rodrigues <142940399+zack-rodrigues@users.noreply.github.com>
2024-07-01 20:58:17 +0000

    mais 4 mini exercícios para a conta.

---
+++ double square area.py
@@ -0,0 +1,4 @@
+area=float(input('Type an area in M² so we can double it: '))
+dobrado= area * 2
+
+print('Your expanded area is:' , dobrado , 'M²')
---
+++ m to cm.py
@@ -0,0 +1,6 @@
+print('Insert an measure in M so we can convert it in to CM :D')
+
+valuem=float(input('Insert an measure in M: '))
+valuecm=(valuem*100)
+
+print('Your measure in CM is: ', valuecm)   
