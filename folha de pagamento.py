5a2b69e60f1a249be4a051d6b9cf1fa07a638f8c
Zack Rodrigues <142940399+zack-rodrigues@users.noreply.github.com>
2024-07-08 21:02:48 +0000

    sliced

--- folha de pagamento.py
+++ folha de pagamento.py
@@ -1,14 +1,20 @@
-#Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês.
-# Calcule e mostre o total do seu salário no referido mês, sabendo-se que são descontados 11% para o Imposto de Renda,
-# 8% para o INSS e 5% para o sindicato, faça um programa que nos dê:
-#
-#salário bruto.
-#quanto pagou ao INSS.
-#quanto pagou ao sindicato.
-#o salário líquido.
-#calcule os descontos e o salário líquido, conforme a tabela abaixo:
-#+ Salário Bruto : R$
-#- IR (11%) : R$
-#- INSS (8%) : R$
-#- Sindicato ( 5%) : R$
-#= Salário Liquido : R$
+ganho_por_hora=float(input('Quanto você ganha por horas trabalhadas: '))
+horas_no_mes=float(input('Quanto você trabalhou no mês: '))
+
+sal_bruto=float(ganho_por_hora*horas_no_mes)
+
+inss=0.08
+sindi=0.05
+ir=0.11
+
+desconto_inss=sal_bruto*inss
+desconto_sindi=sal_bruto*sindi
+desconto_ir=sal_bruto*ir
+
+sal_liquid=sal_bruto -(desconto_inss + desconto_ir + desconto_sindi)
+
+print(f'Seu salário bruto é: R${sal_bruto}')
+print(f'O desconto do INSS foi: R${desconto_inss}')
+print(f'O desconto do sindicato foi: R${desconto_sindi}')
+print(f'O desconto do IR foi: R${desconto_ir}')
+print(f'Seu Salário líquido é de: R${sal_liquid}')
