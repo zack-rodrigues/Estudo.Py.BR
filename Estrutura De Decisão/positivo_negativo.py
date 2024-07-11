7c4c4b73f32b53a503dfe1bedfc1cc6d9686454a
Zack Rodrigues <142940399+zack-rodrigues@users.noreply.github.com>
2024-07-11 19:54:55 +0000

    add ex estrutura de decisão

---
+++ Estrutura De Decisão/positivo_negativo.py
@@ -0,0 +1,15 @@
+num=float(input('Insert an number so we can show if its value is - or +: '))
+
+if num > 0:
+    print('The number is positive..')
+
+if num < 0:
+    print('The number is negative..')
+
+if num == 0:
+    print('zero é nulo...')
+
+
+
+
+
