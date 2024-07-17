575178723fc0b849c3a165f67c3167713f5de393
Zack Rodrigues <zack.empresarial@gmail.com>
2024-07-17 11:05:12 -0300

    teste concluido

--- Estrutura De Decisão/descontos_pagamento.py
+++ Estrutura De Decisão/descontos_pagamento.py
@@ -19,11 +19,14 @@ fgts = salario_bruto * 0.11
 descontos = ir + sindicato
 salario_liquido = salario_bruto - descontos
 
-print(f"Salário Bruto: ({valor_hora:.2f} * {horas_trabalhadas})\t: R$ {salario_bruto:.2f}")
-print(f"(-) IR ({"Isento" if ir == 0 else f"{(ir / salario_bruto) * 100:.0f}%"}):\t\t\tR$ {ir:.2f}")
+if ir == 0:
+    ir_percentual = "Isento"
+else:
+    ir_percentual = f"{(ir / salario_bruto) * 100:.0f}%"
+
+print(f"Salário Bruto:\t\t\t: R$ {salario_bruto:.2f}")
+print(f"(-) IR ({ir_percentual}):\t\t\t: R$ {ir:.2f}")
 print(f"(-) Sindicato (3%)\t\t: R$ {sindicato:.2f}")
 print(f"FGTS (11%)\t\t\t: R$ {fgts:.2f}")
 print(f"Total de descontos\t\t: R$ {descontos:.2f}")
 print(f"Salário Líquido\t\t\t: R$ {salario_liquido:.2f}")
-
-
