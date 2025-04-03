b347405988e070f05db677b0a71baacbb2811b77
zack <zack.empresarial@gmail.com>
2025-04-02 22:40:08 -0300

    mais 4 ex added

---
+++ Estrutura De Funções/soma_tres_numeros.py
@@ -0,0 +1,9 @@
+def tres_arg():
+    a1=int(input('digite um numero primeiro: '))
+    a2=int(input('digite um numero segundo: '))
+    a3=int(input('digite um numero terceiro: '))
+    valores=[a1,a2,a3]
+    soma_valor=sum(valores)
+    print('A soma é',soma_valor)
+
+tres_arg()
