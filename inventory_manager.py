890765f15ccbbf828e6adb8bb3d09a339ebd399c
zack <zack.empresarial@gmail.com>
2025-04-25 15:55:23 -0300

    melhorado sistema de inventario

--- inventory_manager.py
+++ inventory_manager.py
@@ -3,28 +3,39 @@ import csv
 def cadastrar_item():
     with open('database.csv', 'r') as database:
         lista = csv.DictReader(database)
-        ids = [int(linha['id']) for linha in lista]  # Coleta todos os IDs
+        ids = [int(linha['id']) for linha in lista]  # puxa todos id
         new_id = max(ids) + 1 if ids else 1  # Calcula o novo ID e verifica se existem ids na lista, caso não cria o primeiro
 
-    with open('database.csv', 'a') as database:  # Reabre o arquivo em modo de append
-        new_item_name = str(input('Qual nome do item que deseja adicionar? '))
-        new_price = float(input('Qual o preço? '))
-        new_qty = int(input('Qual a quantidade? '))
-        database.write(f'{new_id},{new_item_name},{new_price},{new_qty}\n')
+    with open('database.csv', 'a') as database:  #tem que abrir como append pra dar certo
+        new_item_name = str(input('Digite o nome do item que deseja adicionar: '))
+        new_price = float(input('Digite o preço: '))
+        new_qty = int(input('Digite a quantidade: '))
+        new_brand= str(input('Digite a marca do produto: '))
+        database.write(f'{new_id},{new_item_name},{new_price},{new_qty},{new_brand}\n')
 
 
 def busca_item():
-   with open('database.csv','r') as database:
+    with open('database.csv','r') as database:
+      
       lista=csv.DictReader(database)
-      palavrachave=input('Digite algo para buscar: ').lower()
+      palavrachave=input('Digite o item para buscar: ').lower()
+      marca=input('Digite a marca do produto: ').lower()
+     
+      resultados = []                #essa parte, gera uma lista de resultados apartir desses filros. ordenados por preço.)
       for item in lista:
-         if palavrachave in item['item_name'].lower():
-            print(item)
+        if palavrachave in item['item_name'].lower() and marca in item['brand'].lower():
+           resultados.append(item)
+      
+      resultados_sorted=sorted(resultados, key=lambda x:float(x['price']),reverse=True)
+      for item in resultados_sorted:
+         print(item)
 
 
-cadastrar_item()
+             
+
+
+
 busca_item()
-
 
 
     
