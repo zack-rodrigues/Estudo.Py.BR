9d1618fd787a3838b1a2551cd3972b28ecf26aa5
zack <zack.empresarial@gmail.com>
2025-04-25 17:12:31 -0300

    convertende para pandas

--- inventory_manager.py
+++ inventory_manager.py
@@ -1,16 +1,20 @@
 import csv
+import pandas as pd
 
 def cadastrar_item():
     with open('database.csv', 'r') as database:
+
         lista = csv.DictReader(database)
         ids = [int(linha['id']) for linha in lista]  # puxa todos id
         new_id = max(ids) + 1 if ids else 1  # Calcula o novo ID e verifica se existem ids na lista, caso não cria o primeiro
 
     with open('database.csv', 'a') as database:  #tem que abrir como append pra dar certo
+
         new_item_name = str(input('Digite o nome do item que deseja adicionar: '))
         new_price = float(input('Digite o preço: '))
         new_qty = int(input('Digite a quantidade: '))
         new_brand= str(input('Digite a marca do produto: '))
+
         database.write(f'{new_id},{new_item_name},{new_price},{new_qty},{new_brand}\n')
 
 
@@ -26,17 +30,23 @@ def busca_item():
         if palavrachave in item['item_name'].lower() and marca in item['brand'].lower():
            resultados.append(item)
       
-      resultados_sorted=sorted(resultados, key=lambda x:float(x['price']),reverse=True)
+      resultados_sorted=sorted(resultados, key=lambda x:float(x['price']),reverse=True)  #ordena por preço
       for item in resultados_sorted:
          print(item)
 
 
-             
+def remover_item():
+    id_remove = input('Digite o ID que deseja remover: ')
 
+    # Lê o arquivo CSV em um DataFrame
+    df = pd.read_csv('database.csv', dtype={'id': str})
 
+    # Filtra tudo que NÃO tem o ID
+    df_filtrado = df[df['id'] != id_remove]
 
-busca_item()
+    # Salva de volta no mesmo arquivo CSV
+    df_filtrado.to_csv('database.csv', index=False)
 
+    print(f'Item com ID {id_remove} removido com sucesso (se existia).')
 
-    
-
+remover_item()
